bb92cd7c9894a6bc6f947c57951d8a7bd8b295a7
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-09-03 15:39:11 -0400

    added state

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
+        const { age } = this.state;
 
         return (
             <div>
                 <h2>{lastName}, {firstName}</h2>
                 <p>Age: {age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ () => {this.setState({age: this.state.age + 1})}}>
+                    Birthday Button for {firstName} {lastName}
+                </button>
             </div>
         );
     }
+
+
 }
 
 export default PersonCard;
