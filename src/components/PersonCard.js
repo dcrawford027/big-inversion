39c130ba1fe7ca52da6392e11802b0cd17ce87fa
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-09-04 14:53:35 -0400

    initial commit

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,30 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: this.props.age
-        };
-    }
-
-    render() {
-        const { firstName, lastName, hairColor } = this.props;
-        const { age } = this.state;
-
-        return (
-            <div>
-                <h2>{lastName}, {firstName}</h2>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
-                <button onClick={ () => {this.setState({age: this.state.age + 1})}}>
-                    Birthday Button for {firstName} {lastName}
-                </button>
-            </div>
-        );
-    }
-
-
+const PersonCard = props => {
+    const {lastName, firstName, age, hairColor} = props;
+    return (
+        <div>
+            <h2>{lastName}, {firstName}</h2>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </div>
+    );
 }
+
 
 export default PersonCard;
